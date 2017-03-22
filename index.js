fdac5a05d04ba97311ef667556c1561b17c3515a
sospedra <sospedra.r@gmail.com>
2017-03-22 19:28:46 +0100

    chore: Add bundler, watcher and dev mode

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('hey')
+const { compose, applyMiddleware, createStore } = require('redux')
+const { persistStore, autoRehydrate } = require('redux-persist')
+const createMigration = require('redux-persist-migrate')
